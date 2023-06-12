693e69384af0cd2265639adb17de2c056562b6fb
Md Tasib Haider <hridoyzylex@gmail.com>
2023-06-12 15:46:29 +0600

    Added MVC Pattern on the Destiny App

--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -8,4 +8,36 @@
 
 import Foundation
 
-
+struct StoryBrain {
+    let stories = [
+        Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+        Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+        Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
+    ]
+    
+    var storyNumber = 0
+    
+    func getStoryTitle() -> String {
+        return stories[storyNumber].title
+    }
+    
+    func getChoice1() -> String {
+        return stories[storyNumber].choice1
+    }
+    
+    func getChoice2() -> String {
+        return stories[storyNumber].choice2
+    }
+    
+    mutating func nextStory(_ userChoice: Int) {
+        if userChoice == 1 {
+            
+            storyNumber += 1
+            
+        } else {
+            
+            storyNumber += 2
+            
+        }
+    }
+}
